1594c1be853454c36640b7a909f398ceda069971
Hunter Stone <hunter.m.stone@gmail.com>
2021-12-05 23:56:47 -0700

    updated

--- scene.js
+++ scene.js
@@ -1,5 +1,5 @@
-import * as THREE from './node_modules/three/build/three.module.js';
-import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://cdn.skypack.dev/three@0.135.0';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.135.0/examples/jsm/controls/OrbitControls.js';
 
 let camera, controls, scene, renderer;
 
